a597e13383865a420a7a6e42fa229473e91fecce
kergalym <kegalym2@mail.ru>
2015-08-13 07:12:40 +0600

    Some fixes for app-misc/livebuild-scripts :)

--- app-misc/livebuild-scripts/files/livebuild64_xfce.sh
+++ app-misc/livebuild-scripts/files/livebuild64_xfce.sh
@@ -19,7 +19,7 @@
 ### along with this program; if not, write to the Free Software
 ### Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-configfile="/etc/livebuild-scripts/livebuild.conf"
+configfile="/etc/livebuild-scripts/livebuild64.conf"
 source "$configfile"
 
 if [ "$EUID" -ne 0 ]
